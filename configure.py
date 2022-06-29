8d0c23439bfc43263fc3ac4d4f71504e3ce39109
colored-dye <baoyuntai@outlook.com>
2022-06-29 21:35:03 +0800

    config file init

---
+++ configure.py
@@ -0,0 +1,11 @@
+#-*-coding: UTF-8-*-
+
+import os
+import argparse
+
+BINDIR = os.path.join(os.path.abspath(os.curdir()), 'bin')
+TOPDIR = os.path.dirname(BINDIR)
+
+KERNEL_VERSION = "2.6.11"
+
+
--- src/colinux/os/winnt/user/console-nt/build.comake.py
+++
@@ -1,4 +0,0 @@
-targets['build.a'] = Target(
-    inputs=input_list('.cpp', '.o')
-          +input_list('.c', '.o'),
-)
